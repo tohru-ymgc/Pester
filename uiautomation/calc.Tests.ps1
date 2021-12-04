3bb248fe0eb5e241eb476a5665891cb254462e33
tohru-ymgc <jimmy33hiza@yahoo.co.jp>
2021-12-04 20:26:18 +0900

    新規作成

---
+++ uiautomation/calc.Tests.ps1
@@ -0,0 +1,26 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "動作確認" {
+    Start-Process calc
+    It "電卓ボタン1を押下すると、結果表示が1になること" {
+        click_numbutton_one| Should Be "表示は 1 です"
+    }
+    Stop-Process -name Calculator
+}
+Describe "数字ボタンの動作確認" {
+    Start-Process calc
+    It "電卓ボタン2を押下すると、結果表示が2になること" {
+        click_numbutton_random 2| Should Be "表示は 2 です"
+    }
+    Stop-Process -name Calculator
+}
+
+Describe "add" {
+    Start-Process calc
+    It "2桁の足し算" {
+        add -num1 "10" -num2 "10"| Should Be "表示は 20 です"
+    }
+    Stop-Process -name Calculator
+}
---
+++ uiautomation/calc.ps1
@@ -0,0 +1,37 @@
+﻿Import-Module "UIAutomation.dllが格納されているパス先を指定"
+[UIAutomation.Preferences]::Highlight=$false
+function click_numbutton_one {
+    $wndw = Get-UiaWindow -Name '電卓'
+    $wndw | Get-UiaButton -AutomationId 'num1Button' | Invoke-UiaButtonClick | Out-Null
+    $wndw | Get-UiaText -AutomationID 'CalculatorResults' | Read-UIAControlName
+    Save-UiaScreenshot
+}
+
+function click_numbutton_random ([string]$num) {
+    $wndw = Get-UiaWindow -Name '電卓'
+    $wndw | Get-UiaButton -AutomationId "num$($num)Button" | Invoke-UiaButtonClick | Out-Null
+    $wndw | Get-UiaText -AutomationID 'CalculatorResults' | Read-UIAControlName
+}
+
+function add([string]$num1,[string]$num2) {
+    $i = 0
+    $j = 0
+
+    $wndw = Get-UiaWindow -Name '電卓'
+
+    while([String]::IsNullOrEmpty($num1[$i]) -eq $false)
+    {
+        $wndw | Get-UiaButton -AutomationId "num$($num1[$i])Button" | Invoke-UiaButtonClick | Out-Null
+        $i++
+    }
+
+    $wndw | Get-UiaButton -AutomationId "plusButton" | Invoke-UiaButtonClick | Out-Null
+
+    while([String]::IsNullOrEmpty($num2[$j]) -eq $false)
+    {
+        $wndw | Get-UiaButton -AutomationId "num$($num2[$j])Button" | Invoke-UiaButtonClick | Out-Null
+        $j++
+    }
+    $wndw | Get-UiaButton -AutomationId "equalButton" | Invoke-UiaButtonClick | Out-Null
+    $wndw | Get-UiaText -AutomationID 'CalculatorResults' | Read-UIAControlName
+}
